dacce368b4ea4b419a7d893c744b030dd9db4cfa
zayid <sayidka843@gmail.com>
2023-05-26 20:00:48 +0600

    completed the note app features

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -1,14 +1,71 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from "yup";
 
 // Import Formik and Yup
 
-const AddNote = () => {
+
+const AddNote = (props) => {
  
   // add form logic here
 
+ const {addNotes} = props;
+ 
+const initialValues = {
+  title: "",
+  content:""
+}
+const validationScheme = Yup.object().shape({
+  title: Yup.string().required(),
+  content: Yup.string().required()
+});
+
+const handleSubmit = (values)=>{
+   addNotes({
+     title: values.title ,
+     content: values.content
+  })
+//  console.log(values)
+
+}
+
+ 
+
   return (
-    <div>
+    <div className="flex flex-col flex-wrap justify-start items-center  bg-gray-100 p-8 h-68 w-2/4 space-y-5 rounded-lg" >
       { /* Add here your form and style it with Tailwind */ }
+ 
+
+  
+     <Formik
+     initialValues={initialValues} 
+     onSubmit={handleSubmit} 
+     validationSchema={validationScheme}
+
+     >
+      <Form className="flex flex-col space-y-5 w-full">
+       <Field 
+        type="text" name="title" placeholder="title" 
+        className="w-full rounded border border-b-gray-300 shadow p-3 "
+        />
+       <ErrorMessage name="title"  component="span" className='text-red-500' />
+
+
+       <Field
+        type="text" name="content"  placeholder="body" 
+        className="w-full rounded border border-b-gray-300 shadow p-3"
+        />
+       <ErrorMessage  name="content" component="span"   className='text-red-500' />
+
+       <Field 
+        name="Add Note"    type="submit" 
+        className="rounded mt-4 text-black font-bold   bg-yellow-400  px-12 py-3 "
+        />
+ 
+      </Form>
+
+     </Formik>
+ 
     </div>
   );
 };
--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -2,9 +2,23 @@ import React from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
 
 function Notes(props) {
+  const {notes ,deleteNotes} = props
+  console.log(notes)
   return (
-    <div>
+  <div className="flex flex-wrap items-center justify-center gap-4 mt-6 p-6">
       { /* Style your note cards with Tailwind here and get the data from app.js */ }
+      {notes.map(notes => (
+        <div key={notes.id} className="flex  flex-wrap h-40 w-60 flex-col overflow-x-auto   p-3 rounded bg-yellow-500   "  id="box-note">
+        <div className="h-50 w-50 ">
+        <h1 className=" text-black font-bold   "> {notes.title} </h1>
+         <p className=" text-black text-left pb-3      " > {notes.content}</p>
+        </div>
+         <div className="  flex justify-center items-end ">
+          <FaEdit className="text-black cursor-pointer "/>
+          <FaTrash onClick={() => deleteNotes(notes.id)   } className=" text-black cursor-pointer "/>
+         </div>
+       </div>   
+      ))}
     </div>
   );
 }
