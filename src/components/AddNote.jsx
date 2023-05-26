abf76d1ac5c0437b2f10ccc7f5f201aeda44ce22
unknown <simplecarrier101@gmail.com>
2023-05-25 19:55:35 -0700

    just finished the assignment i added some code for the editnote

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -1,14 +1,62 @@
 import React from 'react';
-
 // Import Formik and Yup
+import { Formik, Form , Field , ErrorMessage} from 'formik';
 
-const AddNote = () => {
- 
+import * as Yup from 'yup';
+
+const AddNote = (props) => {
+
   // add form logic here
 
+  const {addNote} = props;
+
+  const validationSchema = Yup.object({
+    title: Yup.string().required('Add title note'),
+    description: Yup.string().required('Add description note')
+  });
+
+  const initialValues = {
+    title: '',
+    description: ''
+  };
+
+  const handleSubmit = (values) => {
+    addNote({
+      title: values.title,
+      content: values.description
+    });
+
+    values.title = '';
+    values.description = "";
+
+  };
+
   return (
-    <div>
-      { /* Add here your form and style it with Tailwind */ }
+     <div className="p-6 w-[60%] bg-white rounded" id="form-box">
+     <Formik
+       initialValues={initialValues}
+       validationSchema={validationSchema}
+       onSubmit={handleSubmit}
+     >
+      <Form className="p-2 flex flex-col gap-3 ">
+      
+        <Field 
+          type="text"
+          name="title"
+          placeholder="Title"
+           className="w-full p-3 border-2 rounded outline-none"
+          />
+        <ErrorMessage name="title" component="div" className="border-l-4 border-[#ff2727] p-1 text-sm text-[#ff2727] shadow rounded"/>
+         <Field 
+          as="textarea"
+          name="description"
+          placeholder="description"
+          className="w-full p-3 border-2 rounded outline-none"
+         />
+        <ErrorMessage name="description" component="div" className="border-l-4 border-[#ff2727] p-1 text-sm text-[#ff2727] shadow rounded"/>
+         <button type="submit" className="p-4 bg-yellow-400 rounded-lg text-white hover:bg-yellow-500">Add Note</button>
+      </Form>
+     </Formik>
     </div>
   );
 };
--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,12 +1,84 @@
-import React from "react";
+
+import React, { useState } from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
 
 function Notes(props) {
+  const { notes, deleteNote, editNote } = props;
+  const [editingNoteID, setEditingNoteID] = useState(null);
+  const [updatedTitle, setUpdatedTitle] = useState("");
+  const [updatedContent, setUpdatedContent] = useState("");
+
+  const startEditing = (noteID, title, content) => {
+    setEditingNoteID(noteID);
+    setUpdatedTitle(title);
+    setUpdatedContent(content);
+  };
+
+  const cancelEditing = () => {
+    setEditingNoteID(null);
+    setUpdatedTitle("");
+    setUpdatedContent("");
+  };
+
+  const saveNote = (noteID) => {
+    // Call the editNote function with the updated note data
+    editNote(noteID, {
+      title: updatedTitle,
+      content: updatedContent,
+    });
+    cancelEditing();
+  };
+
   return (
-    <div>
-      { /* Style your note cards with Tailwind here and get the data from app.js */ }
+    <div className="flex flex-wrap items-center justify-center gap-4 mt-6 p-6">
+      {notes.map((note) => (
+        <div className="p-4 rounded bg-yellow-500 relative" key={note.id} id="box-note">
+          {editingNoteID === note.id ? (
+            <div>
+              <input
+                type="text"
+                value={updatedTitle}
+                onChange={(e) => setUpdatedTitle(e.target.value)}
+              />
+              <textarea
+                value={updatedContent}
+                onChange={(e) => setUpdatedContent(e.target.value)}
+                className="mt-2"
+              ></textarea>
+              <div className="flex justify-end mt-2">
+                <button
+                  className="px-4 py-2 bg-blue-600 text-white rounded mr-2"
+                  onClick={() => saveNote(note.id)}
+                >
+                  Save
+                </button>
+                <button
+                  className="px-4 py-2 bg-red-600 text-white rounded"
+                  onClick={cancelEditing}
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
+          ) : (
+            <div>
+              <h1 className="text-[#fff] font-bold">{note.title}</h1>
+              <p className="text-white text-left pb-3 overflow-hidden">{note.content}</p>
+              <div className="w-full flex justify-between items-center text-[#fff] ">
+                <FaEdit
+                  onClick={() => startEditing(note.id, note.title, note.content)}
+                  className="cursor-pointer"
+                />
+                <FaTrash onClick={() => deleteNote(note.id)} className="cursor-pointer" />
+              </div>
+            </div>
+          )}
+        </div>
+      ))}
     </div>
   );
 }
 
 export default Notes;
+
+
