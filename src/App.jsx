19fa6e2439876cdc854dbd41eeef71829b9c11b0
Abdulkadir <qansax30@hotmail.com>
2023-07-11 08:50:09 +0200

    waan soo diray

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 // Import all your components here
 // Soo Jiido wixii components ah ood u baahantahay
-
+import AddNote from "./components/AddNote"
+import Notes from "./components/Notes"
 import axios from "axios";
 
 function App() {
@@ -9,20 +10,40 @@ function App() {
 
   useEffect(() => {
     // get all notes from localhost:9000/notes using axios
-    // Dhamaan wixii notes ah kasoo jiido localhost:9000/notes adigoo axios isticmaalaayo
-  }, []);
+    // Dhamaan wixii notes ah kasoo jiido localhost:9000/notes adigoo axios isticmaalaayo 
+    axios.get("htpp://localhost:9000/notes")
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [
+  ]);
+
 
   const createNote = (noteData) => {
     // Make API call to create a note (POST request to localhost:9000/create_note)
     // Halkaas ka samee note cusub adigoo POST request isticmaalaayo localhost:9000/create_note
+    axios.post('http://localhost:9000/create_note', noteData)
+      .then(res => {
+        setNotes([res.data, ...notes]);
+      })
+      .catch(err => {
+        console.log(err);
+      })
   };
+
 
   const deleteNote = (id) => {
     // Make API call to delete a note (DELETE request to localhost:9000/delete_note/:id)
     // Halkaas ka tirtir note adigoo DELETE request isticmaalaayo localhost:9000/delete_note/:id
+
   };
 
-  // STRETCH GOAL: Implement edit functionality
+
+
+// STRETCH GOAL: Implement edit functionality
   // STRETCH GOAL: Isku day inaa edit ku sameyso notes-ka
 
   return (
@@ -30,8 +51,11 @@ function App() {
       <div className="w-full">
         <div className="flex flex-col items-center">
           <h3 className="text-3xl text-white mb-5 mt-5">My Notes</h3>
-          { /* Add here all the components you need */ }
-          { /* Halkaas ku dar components-ka aad u baahan tahay */ }
+          { /* Add here all the components you need */}
+          { /* Halkaas ku dar components-ka aad u baahan tahay */}
+          
+        <AddNote/>
+        <Notes/>
         </div>
       </div>
     </div>
--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,12 +1,31 @@
+// import React from "react";
+// import { FaEdit, FaTrash } from "react-icons/fa";
+
+// function Notes() {
+//   return (
+//     <div>
+//       { /* Style your note cards with Tailwind here and get the data from app.js */ }
+//       <div className="flex flex-wrap items-center justify-center gap-4 mt-6 p-6">
+//        {/* {notes.map(note => (
+//          <div className="p-4 rounded bg-yellow-500 relative" key={note.id} id="box-note">
+//          <h1 className="text-[#fff] font-bold">{note.title}</h1>
+//          <p className="text-white text-left pb-3 overflow-hidden">{note.content}</p>
+//          <div className="w-full flex justify-between items-center text-[#fff] ">
+//           <FaEdit className="cursor-pointer"/>
+//           <FaTrash onClick={() => deleteNote(note.id)} className="cursor-pointer "/>
+//          </div>
+//        </div>   
+//        ))}       */}
+//     </div>
+//   );
+// }
+  
 import React from "react";
-import { FaEdit, FaTrash } from "react-icons/fa";
-
-function Notes(props) {
+function Notes() { 
   return (
-    <div>
-      { /* Style your note cards with Tailwind here and get the data from app.js */ }
-    </div>
-  );
+    <h1>hello world </h1>
+  )
 }
+export default Notes
 
-export default Notes;
+
